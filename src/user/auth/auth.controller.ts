4df2e98bb512ca81e268dfc6181b6378eef80f94
신승훈 <>
2022-05-23 22:25:33 +0900

    feature : user input
    - post body dto 생성

---
+++ src/user/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { SignupDto } from '../dtos/auth.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signup(@Body() body: SignupDto) {
+    return this.authService.signup();
+  }
+}
---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Matches,
+} from 'class-validator';
+
+export class SignupDto {
+  @IsString({ message: '문자만 가능합니다.' })
+  @IsNotEmpty({ message: '반드시 이름을 입력해야합니다.' })
+  name: string;
+
+  @Matches(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/, {
+    message: '올바른 휴대폰 형식을 입력하세요.',
+  })
+  phone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString({ message: '문자만 가능합니다.' })
+  @MinLength(5, { message: '최소 5자리 입력해야합니다.' })
+  password: string;
+}

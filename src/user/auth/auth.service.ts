ff3665ff568a21ee4b6368417e9d6096ac8abb2f
신승훈 <>
2022-05-23 22:50:14 +0900

    feature : email validate
    - 이메일에 유니크 설정
    - db에 있으면 exception 처리

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
 
 @Injectable()
 export class AuthService {
-  signup() {}
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email }: SignupParams) {
+    // 유니크한지 체크하려면
+    // 스키마에 @unique 가 들어가있어야함
+
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException();
+    }
+  }
 }
